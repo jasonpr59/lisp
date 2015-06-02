bd28a4c37f795ab307f6b3fe9f9a35271dbe8b82
Jason Paller-Rzepka <jasonpr@mit.edu>
2015-06-02 13:36:45 -0400

    Test corecursion in `test/tail-call.lisp`.

--- test/tail-call.lisp
+++ test/tail-call.lisp
@@ -2,6 +2,22 @@
   (lambda (n)
     (begin
      (print! n)
-     (count (+ n 1)))))
+     (if (< n 10000)
+	 (count (+ n 1))
+       n))))
 
 (count 0)
+
+(define count-a
+  (lambda (n)
+    (if (< n 20000)
+	(count-b (+ n 1))
+      n)))
+
+(define count-b
+  (lambda (n)
+    (begin
+     (print! n)
+     (count-a (+ n 1)))))
+
+(count-a 0)
