778560bc3a47932188c817e7ef052cedfc629a19
Jason Paller-Rzepka <jasonpr@mit.edu>
2015-06-02 21:11:02 -0400

    Transition to new function syntax in the tail-call test.

--- test/tail-call.lisp
+++ test/tail-call.lisp
@@ -1,21 +1,15 @@
-(define count
-  (lambda (n)
-     (print! n)
-     (if (< n 10000)
-	 (count (+ n 1))
-       n)))
-
+(define (count n)
+  (print! n)
+  (if (< n 10000)
+      (count (+ n 1))
+    n))
 (count 0)
 
-(define count-a
-  (lambda (n)
-    (if (< n 20000)
-	(count-b (+ n 1))
-      n)))
-
-(define count-b
-  (lambda (n)
-     (print! n)
-     (count-a (+ n 1))))
-
+(define (count-a n)
+  (if (< n 20000)
+      (count-b (+ n 1))
+    n))
+(define (count-b n)
+  (print! n)
+  (count-a (+ n 1)))
 (count-a 0)
